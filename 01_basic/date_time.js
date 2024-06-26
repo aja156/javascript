7a6ca32ae9fd97e86211d21eb0b6567ce5c184c6
aja156 <ak7129675@gmail.com>
2024-06-26 08:35:37 +0000

    date and time

---
+++ 01_basic/date_time.js
@@ -0,0 +1,20 @@
+let todate=new Date();
+// console.log(todate.toDateString());
+// console.log(Date());
+
+// let nowdate =new Date("10-25-2020");
+// console.log(nowdate.toString());
+// console.log(todate.toLocaleDateString());  // it tels in the form of 2/25/2024
+// console.log(todate.toISOString());  //it tels date in the form of 2024-6-26
+// console.log(todate.toJSON());   //it tels date in the form of 2024-6-26
+// console.log(todate.toLocaleTimeString()); //it tels the time
+// console.log(todate.toTimeString()); //it tels the time 
+
+// let mynewdate = new Date(2024,0,23);
+// console.log(mynewdate.toDateString());
+
+// let creatmydate =Date.now();
+// console.log(creatmydate);
+console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
