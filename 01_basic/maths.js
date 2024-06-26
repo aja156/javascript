58ba528d8f2c04ef5c700f0f9c2345c01f0315e8
aja156 <ak7129675@gmail.com>
2024-06-26 16:41:48 +0000

    array day1

---
+++ 01_basic/maths.js
@@ -0,0 +1,18 @@
+let score =400;
+console.log(score);
+
+let newscore=new Number(200);
+// console.log(newscore);
+
+console.log(newscore.toString());
+// console.log(typeof newscore);
+
+let othernumber=245.6985;
+// console.log(othernumber.toPrecision(5));
+
+// console.log(Math.round(4.9));
+// console.log(Math.random());
+
+console.log(math.floor(4.5));
+
+
---
+++ 02_basic/array.js
@@ -0,0 +1,14 @@
+const myary=[1,2,3,4];
+console.log(myary[2]);
+
+const newary = new Array(1,2,3,4);
+console.log(newary[2]);
+
+// newary.push(5);
+newary.pop();
+console.log(newary);
+
+
+ newary.unshift(5);
+newary.shift(5);
+console.log(newary);
