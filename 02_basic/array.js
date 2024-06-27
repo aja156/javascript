c1525fa63a67aa996cd43a61057223806e7553f8
aja156 <ak7129675@gmail.com>
2024-06-27 12:35:13 +0000

    array class 2nd

--- 02_basic/array.js
+++ 02_basic/array.js
@@ -1,14 +1,32 @@
 const myary=[1,2,3,4];
-console.log(myary[2]);
+// console.log(myary[2]);
 
-const newary = new Array(1,2,3,4);
-console.log(newary[2]);
+const newary = new Array(1,2,3,4,5,6);
+// console.log(newary[2]);
 
 // newary.push(5);
 newary.pop();
-console.log(newary);
+// console.log(newary);
 
 
  newary.unshift(5);
 newary.shift(5);
-console.log(newary);
+// console.log(newary);
+// console.log(newary.includes(8));
+// console.log(newary.indexOf(8));
+
+console.log("A",newary);
+
+ const narry= newary.slice(0,3);
+
+console.log(narry);
+
+console.log("B",newary);
+
+let slplicearr =Array(1,2,3,4,5,6,);
+console.log(slplicearr);
+const sparry =slplicearr.splice(1,3);
+console.log(sparry);
+
+
+
