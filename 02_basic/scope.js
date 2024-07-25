8473c08bb25adf71364681699b8d2053805eb20b
aja156 <ak7129675@gmail.com>
2024-07-25 14:55:49 +0000

    scope

---
+++ 02_basic/scope.js
@@ -0,0 +1,20 @@
+function addtwo(num){
+    return num+1;
+}
+//  console.log(addtwo(10));
+
+ const two=function(num){
+   return num+5;
+ }
+//  console.log(two(5));
+
+ function three(){
+    const name="ajay"
+    function one(){
+        const username="kumar"
+        console.log(username);
+
+    }
+
+ }
+//  two();
