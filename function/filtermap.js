373bea7735db6802b497a2cb0d51aa79f77b64e9
aja156 <ak7129675@gmail.com>
2024-08-15 06:37:27 +0000

    filtermap

---
+++ function/filtermap.js
@@ -0,0 +1,13 @@
+// const coding=["ja","php","python","laravel"];
+
+// coding.forEach( (item)=>{
+  
+//     console.log(item);
+// } )
+
+//filter 
+
+const mynum=[1,2,3,4,5,6,7,8,9,10];
+
+const mynew=mynum.filter((mynum)=> mynum>5 )
+console.log(mynew);
